fb8a115064472a79376a8e531183c1ac29289ec5
Christian Bunk <christianb.github@googlemail.com>
2011-12-10 12:27:43 +0100

    add patient detail view

--- Android/src/com/activities/PatentDetailView.java
+++ Android/src/com/activities/PatentDetailView.java
@@ -1,6 +1,15 @@
 package com.activities;
 
-/** Patientendetails: Name, Geburtstag, etc. */
-public class PatentDetailView {
+import android.app.Activity;
+import android.os.Bundle;
 
+/** Patientendetails: Name, Geburtstag, etc. */
+public class PatentDetailView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.patient_detail);
+	}
 }
