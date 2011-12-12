08ab0e54986005e955559b3f1f09320416a4af78
Christian Bunk <christianb.github@googlemail.com>
2011-12-12 20:46:44 +0100

    add Font

--- Android/src/com/activities/Impressum.java
+++ Android/src/com/activities/Impressum.java
@@ -1,7 +1,9 @@
 package com.activities;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Impressum extends Activity {
 
@@ -10,5 +12,20 @@ public class Impressum extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.impressum);
+		setLayoutFont();
+	}
+	
+	public void setLayoutFont() {
+	    Typeface tf = Typeface.createFromAsset(
+	        getBaseContext().getAssets(), "fonts/SegoeWP-Light.ttf");
+	    TextView tv = (TextView)findViewById(R.id.impressumg_headline);
+	    tv.setTypeface(tf);
+	    
+	    tv = (TextView)findViewById(R.id.impressum_text_1);
+	    tv.setTypeface(tf);
+	    
+	    tv = (TextView)findViewById(R.id.impressum_text_2);
+	    tv.setTypeface(tf);
+
 	}
 }
